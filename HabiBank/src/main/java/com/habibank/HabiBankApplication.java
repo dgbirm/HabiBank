1692f63ff8bbbc5a1d9311ecd6958437957a30b8
Daniel Birmingham <dgbirm@gmail.com>
2020-08-28 14:14:06 -0400

    Add files via upload

---
+++ HabiBank/src/main/java/com/habibank/HabiBankApplication.java
@@ -0,0 +1,13 @@
+package com.habibank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HabiBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HabiBankApplication.class, args);
+	}
+
+}
---
+++ HabiBank/src/test/java/com/habibank/HabiBankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.habibank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HabiBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
