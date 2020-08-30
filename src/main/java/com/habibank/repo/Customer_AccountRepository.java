b3f8982e1d2e4490a33749f68f87f6941eb9042b
Dan Birmingham <dgbirm@gmail.com>
2020-08-29 22:17:13 -0400

    update Account to reflect new framework
    were going to leverage the fact that jpa can persist Sets to manage which customer is listed on an account

--- src/main/java/com/habibank/repo/Customer_AccountRepository.java
+++ src/main/java/com/habibank/repo/Customer_AccountRepository.java
@@ -3,6 +3,8 @@ package com.habibank.repo;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+import com.habibank.model.Customer_Account;
+
 @RepositoryRestResource
 public interface Customer_AccountRepository extends JpaRepository<Customer_Account, String> {
 	
