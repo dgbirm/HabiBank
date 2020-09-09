94e3f287cc489dede04bd6aba3c6822ebda47ba9
natashang <50751197+natashang@users.noreply.github.com>
2020-09-08 19:45:16 -0700

    [redux, axios] : base retrieve all customers

--- src/main/web-app/src/redux/reducers/index.js
+++ src/main/web-app/src/redux/reducers/index.js
@@ -3,9 +3,11 @@ import { combineReducers } from "redux";
 import checkings from "./checkings";
 import customer from "./customer";
 import savings from "./savings";
+import loading from "./loading";
 
 export default combineReducers({
   checkings,
   customer,
   savings,
+  loading,
 });
