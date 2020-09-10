c8d32875575edc47b48355944f5829bd2c6c2301
natashang <50751197+natashang@users.noreply.github.com>
2020-09-09 18:52:36 -0700

    [UI] account home view updated with table

--- src/main/web-app/src/redux/reducers/customer.js
+++ src/main/web-app/src/redux/reducers/customer.js
@@ -4,12 +4,26 @@ import { LOAD_SINGLE_CUSTOMER } from "../actions/actionTypes";
  */
 
 const initialState = {
-  userName: "jdoe",
-  fullName: "Jane Doe",
-  email: "jd@doeeee.com",
-  address: "Disneyland, CA",
-  phoneNumber: "1234567890",
-  customerID: 1,
+  profile: {
+    userName: "jdoe",
+    fullName: "Jane Doe",
+    email: "jd@doeeee.com",
+    address: "Disneyland, CA",
+    phoneNumber: "1234567890",
+    customerID: 1,
+  },
+  accounts: [
+    {
+      accountID: 1,
+      accountBalance: 900.1,
+      accountType: "checkings",
+    },
+    {
+      accountID: 2,
+      accountBalance: 222.47,
+      accountType: "savings",
+    },
+  ],
   userLoaded: true, //false, // currently hard coded
 };
 
