f4e816487e390386e81a7be6f62603ecaaafc48d
natashang <50751197+natashang@users.noreply.github.com>
2020-09-09 18:52:36 -0700

    [UI] account home view updated with table

--- src/main/web-app/src/components/AccountHome/AccountHome.js
+++ src/main/web-app/src/components/AccountHome/AccountHome.js
@@ -1,29 +1,85 @@
 import React from "react";
 import "./AccountHome.css";
 import { connect } from "react-redux";
-import { Button } from "react-bootstrap";
+import { Button, Table } from "react-bootstrap";
 import { Link } from "react-router-dom";
+import {
+  ACCOUNT_HOME,
+  ACCOUNT_HOME_MESSAGE,
+  BANK_ACCOUNT_ID,
+  ACCOUNT_TYPE,
+  SAVINGS,
+  CHECKINGS,
+  BALANCE,
+} from "../../constants/index";
 /**
  * Home page when a user successfully logs in
  * Multiple user routes can be taken here (reference user flow diagram)
  */
-const AccountHome = () => {
+const AccountHome = (props) => {
+  const renderMessages = () => {
+    return (
+      <div className="messages">
+        <h2>{ACCOUNT_HOME}</h2>
+        <h6>
+          <i>{ACCOUNT_HOME_MESSAGE}</i>
+        </h6>
+      </div>
+    );
+  };
+
+  const renderBalanceView = () => {
+    return (
+      <div className="form-wrapper customStyle">
+        <Table striped borderless hover>
+          <thead>
+            <tr>
+              <th>{BANK_ACCOUNT_ID}</th>
+              <th>{ACCOUNT_TYPE}</th>
+              <th>{BALANCE}</th>
+            </tr>
+          </thead>
+          <tbody>
+            {props.accounts.map((each, index) => {
+              return (
+                <tr key={index}>
+                  <td>{each.accountID}</td>
+                  <td>{each.accountType.toUpperCase()}</td>
+                  <td>${each.accountBalance.toFixed(2)}</td>
+                  <td>{renderButton(each.accountType.toUpperCase())}</td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </Table>
+      </div>
+    );
+  };
+
+  const renderButton = (type) => {
+    let link = type === "SAVINGS" ? "/savings" : "/checkings";
+    let text = type === "SAVINGS" ? SAVINGS : CHECKINGS;
+
+    return (
+      <Link to={link}>
+        <Button variant="primary">{text}</Button>
+      </Link>
+    );
+  };
+
   return (
     <div>
-      <h3>Account Home</h3>
-      <Link to="/savings">
-        <Button variant="primary">Savings</Button>
-      </Link>
-      <Link to="/checkings">
-        <Button variant="primary">Checkings</Button>
-      </Link>
+      {renderMessages()}
+      {renderBalanceView()}
     </div>
   );
 };
 
-// export default AccountHome;
 const mapStateToProps = (state) => {
-  return {};
+  const { customer } = state;
+  return {
+    accounts: customer.accounts,
+  };
 };
 
 const mapDispatchToProps = {};
--- src/main/web-app/src/constants/index.js
+++ src/main/web-app/src/constants/index.js
@@ -1,8 +1,18 @@
-// Messages
+// General
 export const BANK_NAME = "Habi Bank";
 export const LOGIN_SLOGAN = "Reliable banking you can trust";
 
-// Labels
+// Account Home
+export const ACCOUNT_HOME = "Account Home";
+export const ACCOUNT_HOME_MESSAGE =
+  "This is your dashboard to access your account balance, transactions, and user information";
+export const BANK_ACCOUNT_ID = "Account #";
+export const ACCOUNT_TYPE = "Account Type";
+export const SAVINGS = "Savings";
+export const CHECKINGS = "Checkings";
+export const BALANCE = "Balance";
+
+// Form Labels
 export const USERNAME = "Username";
 export const PASSWORD = "Password";
 export const EMAIL = "Email Address";
@@ -10,7 +20,7 @@ export const NAME = "Name";
 export const ADDRESS = "Address";
 export const PHONE_NUMBER = "Phone Number";
 
-// Interaction
+// Form Interaction
 export const ENTER_EMAIL = "Enter email";
 export const SUBMIT = "Submit";
 export const CREATE_ACCOUNT_TODAY = "Create an account today!";
