e073b8b12e11ef487167848d210226393582bc49
ChrisJabb21 <chrisjabbour94@gmail.com>
2020-09-14 15:37:29 -0400

    made some changes to home controller, removed webpack

--- src/main/java/com/habibank/controller/HomeController.java
+++ src/main/java/com/habibank/controller/HomeController.java
@@ -1,13 +1,32 @@
 package com.habibank.controller;
 
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@CrossOrigin(origins = "http://localhost:3000")
 @Controller
+@Profile("prod")
 public class HomeController {
-    	//Not sure if we need a custom UI controller in different contorller  
-	@RequestMapping(value = "/") //flag '', '/', 'index' to support [react-app] index
-	private String index() {
-		return "index";
+		//Not sure if we need a custom UI controller in different contorller  
+		
+		@GetMapping("/") //flag '', '/', 'index' to support [react-app] index
+		private String getRedirectToProile() {
+			return "redirect:/";
+		}
+		
+	@GetMapping("/profile") //flag '', '/', 'index' to support [react-app] index
+	private String getRedirectToProfile() {
+		return "redirect:/profile";
 	}
+
+	@GetMapping("/register")
+	private String redirectToRegister() {
+		return "redirect:/register";
+	}
+	
 }
